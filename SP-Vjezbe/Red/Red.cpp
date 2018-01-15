251fe83300f1ae68990d3bdf337dfbde8c51336e
fsr-sp <danijel.zelenika@sve-mo.ba>
2018-01-15 11:56:58 +0100

    Red

---
+++ SP-Vjezbe/Red/Red.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define MAX 10
+
+typedef struct s {
+	int niz[MAX];
+	int ulaz;
+	int izlaz;
+} Red;
+
+void init(Red *red) {
+	red->ulaz =  -1;
+	red->izlaz = -1;
+}
+
+int dodaj(Red *red, int broj) {
+	return 1;
+}
+
+int skini(Red *red,  int *broj) {
+	return 1;
+}
+
+void main() {
+
+}
